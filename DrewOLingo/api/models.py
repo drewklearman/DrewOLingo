66de84caaf53ecada20594d39bb93c0ecc398855
drewklearman <drew.klearman@gmail.com>
2025-04-30 15:09:10 -0700

    first API endpoint finished

--- DrewOLingo/api/models.py
+++ DrewOLingo/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import random
+
+def generate_chat_bot_name():
+    return "ChatBot " + random.choice(["Tom", "Jerry", "John", "Jane", "Jim", "Jill"])
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    password = models.CharField(max_length=255)
+    level = models.IntegerField()
+    chat_bot_name = models.CharField(max_length=255)
+    
+
--- DrewOLingo/api/views.py
+++ DrewOLingo/api/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from rest_framework import generics
+from .serializers import UserSerializer
+from .models import User
 # Create your views here.
 
-def main(request):
-    return HttpResponse("Hello, World!")
+class UserView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
 
